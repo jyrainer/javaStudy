2327181fb8fff8eb0c5ffc7191ac5eaf5f496655
jyrainer <jyrainer@snuailab.ai>
2024-10-04 20:55:20 +0900

    add 2.variable

---
+++ src/variable/VarEx2.java
@@ -0,0 +1,16 @@
+package variable;
+
+/*
+변수 num1을 선언하고 10을 할당
+변수 num2를 선언하고 20을 할당
+두 변수의 합을 구하고, 그 결과를 새로운 변수 sum에 저장
+sum변수의 값을 출력
+ */
+public class VarEx2 {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
